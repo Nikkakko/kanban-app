eb3d788f62619eda194df9485f24c84f1311e438
Nikkakko <nikolozkopadze@gmail.com>
2023-02-16 08:35:05 +0400

    updated

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -80,7 +80,7 @@ const MainContainer = styled.div`
   display: flex;
   flex-direction: column;
   width: 100%;
-  height: 100vh;
+  height: 100%;
 
   @media (max-width: 768px) {
     overflow: auto;
